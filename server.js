941b331e22f9da4e220477ca1e8fe57cce1d9b2f
Steve <stvchrm@steves-mbp.lan>
2024-06-08 11:42:44 -0600

    Initial project setup with basic configuration

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-// server.js
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -20,7 +18,11 @@ app.use(session({
   saveUninitialized: true
 }));
 
-app.use(routes);
+// Placeholder for routes
+app.use('/auth', (req, res) => res.send('Auth route placeholder'));
+app.use('/books', (req, res) => res.send('Books route placeholder'));
+app.use('/clubs', (req, res) => res.send('Clubs route placeholder'));
+app.use('/meetings', (req, res) => res.send('Meetings route placeholder'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
