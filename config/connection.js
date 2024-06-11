c655cb717578e4291b1fd654d8cd2b22a7faf48a
Steve <stvchrm@steves-mbp.lan>
2024-06-10 21:17:41 -0600

    Setup database connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        port: 5432
+      }
+    );
+
+module.exports = sequelize;
