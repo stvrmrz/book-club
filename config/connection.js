ee3aa5edce6e672a961bfa8e543737c37cc8ca8a
Steve <stvchrm@steves-mbp.lan>
2024-06-11 07:30:57 -0600

    deploy to render

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,18 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // This is important for self-signed certificates
+      }
+    },
+    logging: console.log // Optional: You can set this to false to disable logging
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -11,7 +21,14 @@ if (process.env.DB_URL) {
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
-      dialect: 'postgres'
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      logging: console.log // Optional: You can set this to false to disable logging
     }
   );
 }
