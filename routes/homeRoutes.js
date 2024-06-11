7dcda7e9de5061993e48b5fe6d45118e43439eb8
Steve <stvchrm@steves-mbp.lan>
2024-06-11 08:18:51 -0600

    Fix attempt for render

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('pages/home', { title: 'Home' }); // Adjusted path
+  res.render('pages/home', { title: 'Home' });
 });
 
 module.exports = router;
