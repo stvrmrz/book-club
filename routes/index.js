14cfeec9519954ffaf41f4e7eb77907786936c27
Steve <stvchrm@steves-mbp.lan>
2024-08-03 16:58:26 -0600

    update routes index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const authRoutes = require('./authRoutes');
 const homeRoutes = require('./homeRoutes');
+const clubRoutes = require('./clubRoutes'); // Import club routes
 
 // Use auth routes for authentication-related endpoints
 router.use('/auth', authRoutes);
+
+// Use club routes for the clubs page
+router.use('/clubs', clubRoutes);
 
 // Use home routes for the home page
 router.use('/', homeRoutes);
