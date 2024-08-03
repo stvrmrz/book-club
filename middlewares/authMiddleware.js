274f92ef3922775c99be9a08d811cb9151896e8f
Steve <stvchrm@steves-mbp.lan>
2024-08-03 16:56:41 -0600

    add middleWare

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,10 @@
+const isLoggedIn = (req, res, next) => {
+    if (req.session.loggedIn) {
+      next();
+    } else {
+      res.redirect('/auth/login');
+    }
+  };
+  
+  module.exports = { isLoggedIn };
+  
