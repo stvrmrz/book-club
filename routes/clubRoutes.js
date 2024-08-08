826b16b34920a40e85a5c075958e9779e6195f48
Steve <stvchrm@steves-mbp.lan>
2024-08-08 17:43:23 -0600

    Fixed adding club in handlebar

--- routes/clubRoutes.js
+++ routes/clubRoutes.js
@@ -5,6 +5,7 @@ const { Club } = require('../models');
 // Route to get all clubs
 router.get('/', isLoggedIn, async (req, res) => {
   try {
+    // Fetch all clubs without filtering by userId
     const clubs = await Club.findAll();
     res.render('pages/clubs', { title: 'Book Clubs', clubs });
   } catch (error) {
@@ -13,18 +14,18 @@ router.get('/', isLoggedIn, async (req, res) => {
   }
 });
 
-// Route to render the form for adding a new club
+// Route to render the form for creating a new club
 router.get('/new', isLoggedIn, (req, res) => {
   res.render('pages/newClub', { title: 'New Club' });
 });
 
 // Route to create a new club
-router.post('/', isLoggedIn, async (req, res) => {
+router.post('/new', isLoggedIn, async (req, res) => {
   try {
     const newClub = await Club.create({
       name: req.body.name,
       description: req.body.description,
-      userId: req.session.user.id // Ensure the club is associated with the logged-in user
+      userId: req.session.user.id, // Associate club with the logged-in user
     });
     res.redirect('/clubs');
   } catch (error) {
